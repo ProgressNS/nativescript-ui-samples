ef7fbd5d98813884d362051647990da7b7fcd777
Deyan Ginev <deyan.ginev@telerik.com>
2016-02-04 13:48:25 +0200

    Added ExamplePage with Action Bar title

--- sdk/app/navigation/example-page.js
+++ sdk/app/navigation/example-page.js
@@ -4,6 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var pageModule = require("ui/page");
+var actionBarModule = require("ui/action-bar");
 var ExamplePage = (function (_super) {
     __extends(ExamplePage, _super);
     function ExamplePage() {
@@ -12,9 +13,12 @@ var ExamplePage = (function (_super) {
         this.on("navigatingTo", function (args) {
             that.get()._associatedExampleMeta = args.context;
         });
+        var customActionBar = new actionBarModule.ActionBar();
+        this.actionBar = customActionBar;
     }
     ExamplePage.prototype._onBindingContextChanged = function (oldValue, newValue) {
         newValue["exampleContext"] = this._associatedExampleMeta;
+        this.actionBar.title = this._associatedExampleMeta.title;
         _super.prototype._onBindingContextChanged.call(this, oldValue, newValue);
     };
     return ExamplePage;
--- sdk/app/navigation/example-page.ts
+++ sdk/app/navigation/example-page.ts
@@ -1,4 +1,5 @@
 import pageModule = require("ui/page");
+import actionBarModule = require("ui/action-bar");
 
 export class ExamplePage extends pageModule.Page {
 
@@ -10,10 +11,20 @@ export class ExamplePage extends pageModule.Page {
         this.on("navigatingTo", function(args) {
             that.get()._associatedExampleMeta = args.context;
         });
+        var customActionBar = new actionBarModule.ActionBar();
+        // var bindingOptions = {
+        //     sourceProperty: "bindingContext.exampleContext.title",
+        //     targetProperty: "title"
+        // };
+
+        this.actionBar = customActionBar;
+
+        // customActionBar.bind(bindingOptions, this);
     }
 
     public _onBindingContextChanged(oldValue, newValue) {
         newValue["exampleContext"] = this._associatedExampleMeta;
+        this.actionBar.title = this._associatedExampleMeta.title;
         super._onBindingContextChanged(oldValue, newValue);
     }
 }
