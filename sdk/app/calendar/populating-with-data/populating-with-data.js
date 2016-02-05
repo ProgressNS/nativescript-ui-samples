82e0e97ee4ac842bde431e1aa10456af0e08f08d
guliashki <adrian.guliashki@gmail.com>
2016-02-05 11:02:53 +0200

    Updates in examples

--- sdk/app/calendar/populating-with-data/populating-with-data.js
+++ sdk/app/calendar/populating-with-data/populating-with-data.js
@@ -1,6 +1,12 @@
 var viewModel = require("./populating-with-data-model");
+var viewModelContext;
 function onPageLoaded(args) {
     var page = args.object;
-    page.bindingContext = new viewModel.ViewModel();
+    viewModelContext = new viewModel.ViewModel();
+    page.bindingContext = viewModelContext;
 }
 exports.onPageLoaded = onPageLoaded;
+function onDateSelected(args) {
+    viewModelContext.onDateSelected(args);
+}
+exports.onDateSelected = onDateSelected;
