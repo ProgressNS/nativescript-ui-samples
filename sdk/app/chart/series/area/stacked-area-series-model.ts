09bd9a2574bb766e632720438f58351391d23d67
Yoanna Mareva <mareva@localuitestexec.telerik.com>
2016-03-09 15:22:59 +0200

    added options to chart stacked area example

---
+++ sdk/app/chart/series/area/stacked-area-series-model.ts
@@ -0,0 +1,49 @@
+import {ObservableArray} from "data/observable-array";
+import chartModule = require("nativescript-telerik-ui-pro/chart");
+import observableModule = require("data/observable");
+import frameModule = require("ui/frame");
+
+export class ViewModel extends observableModule.Observable {
+
+    private _stackModes;
+
+    constructor() {
+        super();
+      
+        this._stackModes = {
+            options: ["Stack", "Stack100", "None"],
+            index: 0
+        };
+    }
+
+    public onOptionsTapped() {
+        var navigationEntry = {
+            moduleName: "/navigation/options-menu/options",
+            context: this._stackModes,
+            animated: true
+        };
+
+        frameModule.topmost().navigate(navigationEntry);
+    }
+    
+     public updateStackMode() {
+        var index: number = this._stackModes.index;
+        let b = this._stackModes.options[index];
+        var chart = <chartModule.RadCartesianChart>frameModule.topmost().currentPage.getViewById("cartesianChart");
+        chart.series[0].stackMode = this._stackModes.options[index];
+         chart.series[1].stackMode = this._stackModes.options[index];
+          chart.series[2].stackMode = this._stackModes.options[index];
+        
+    }
+    
+     get categoricalSource() {
+        return [
+            { Country: "Germany", Amount: 15, SecondVal: 14, ThirdVal: 24 },
+            { Country: "France", Amount: 13, SecondVal: 23, ThirdVal: 25 },
+            { Country: "Bulgaria", Amount: 24, SecondVal: 17, ThirdVal: 23 },
+            { Country: "Spain", Amount: 5, SecondVal: 19, ThirdVal: 24 },
+            { Country: "USA", Amount: 18, SecondVal: 8, ThirdVal: 21 }
+        ]
+    }
+
+}
--- sdk/app/chart/series/area/stacked-area-series.ts
+++ sdk/app/chart/series/area/stacked-area-series.ts
@@ -1,6 +1,42 @@
 import categoricalDataModelModule = require("../../data-models/categorical-data-model");
+import stackedSeriesModel = require("./stacked-area-series-model");
+import chartModule = require("nativescript-telerik-ui-pro/chart");
+import frameModule = require("ui/frame");
 
+var context = new stackedSeriesModel.ViewModel();
 export function onPageLoaded(args){
     var page = args.object;
-    page.bindingContext = new categoricalDataModelModule.CategoricalDataModel();
+    page.bindingContext = context;
+}
+
+export function onNavigatedTo(args) {
+    console.log(this.context);
+   context.updateStackMode();
+}
+
+export function onNavigatedFrom(args) {
+    if (args.isBackNavigation === true) {
+        context = undefined;
+    }
+}
+
+export function onNoneStackModeSelected(args: any) {
+    var chart = <chartModule.RadCartesianChart>frameModule.topmost().getViewById("cartesianChart");
+    for (var series in chart.series) {
+       series.stackMode = "None";
+    }
+}
+
+export function onStackModeSelected(args: any) {
+        var chart = <chartModule.RadCartesianChart>frameModule.topmost().getViewById("cartesianChart");
+    for (var series in chart.series) {
+       series.stackMode = "Stack";
+    }
+}
+
+export function onStack100ModeSelected(args: any) {
+        var chart = <chartModule.RadCartesianChart>frameModule.topmost().getViewById("cartesianChart");
+    for (var series in chart.series) {
+       series.stackMode = "Stack100";
+    }
 }
