99e2aee26931ae3a3cf3a2634cf0639feec10e38
karaahmed <karaahmed@gmail.com>
2016-03-23 16:37:50 +0200

    Added inline events style & updated events to be shown for Android

--- sdk/app/calendar/cell-styling/cell-styles-model.ts
+++ sdk/app/calendar/cell-styling/cell-styles-model.ts
@@ -22,7 +22,7 @@ export class StylingViewModel extends Observable {
         let events: Array<calendarModule.CalendarEvent> = new Array<calendarModule.CalendarEvent>();
         for (let i = 1; i < 10; i++) {
             startDate = new Date(now.getFullYear(), now.getMonth(), i * 2);
-            endDate = new Date(now.getFullYear(), now.getMonth(), (i * 2) + (i % 3));
+            endDate = new Date(now.getFullYear(), now.getMonth(), (i * 2), 3);
             event = new calendarModule.CalendarEvent("event " + i, startDate, endDate, false, colors[i * 10 % (colors.length - 1)]);
             events.push(event);
             if (i % 3 == 0) {
