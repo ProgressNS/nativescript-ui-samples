0f3d7f14e4c3e11a4beace826ddddfa02fbefb64
Deyan Ginev <deyan.ginev@telerik.com>
2016-03-28 14:29:22 +0300

    Extended events examples

---
+++ sdk/app/calendar/calendar-events/events-source-model.ts
@@ -0,0 +1,33 @@
+import observableModule = require("data/observable");
+// >> calendar-calendar-require
+import calendarModule = require("nativescript-telerik-ui-pro/calendar");
+// << calendar-calendar-require
+import frameModule = require("ui/frame");
+
+// >> events-source-model
+export class ViewModel extends observableModule.Observable {
+    private _eventTitles: Array<string> = ["Meeting with Jack", "Lunch with Peter", "Planning meeting",
+                                            "Go shopping", "Very important meeting", "Another meeting", "Random event"];
+ 
+    constructor() {
+        super();
+        this.createEvents();
+    }
+    
+    // >> calendar-calendar-event-instance
+    public createEvents() {
+        let events: Array<calendarModule.CalendarEvent> = new Array<calendarModule.CalendarEvent>();
+        let j = 1;
+        for (let i = 0; i < this._eventTitles.length; i++) {
+            var now: Date = new Date();
+            var startDate: Date = new Date(now.getFullYear(), now.getMonth(), j * 2);
+            var endDate: Date = new Date(now.getFullYear(), now.getMonth(), (j * 2) + (j % 3));
+			var event = new calendarModule.CalendarEvent(this._eventTitles[i], startDate, endDate);
+			events.push(event);
+            j++;
+		}
+        this.set("calendarEvents", events);
+    }
+    // << calendar-calendar-event-instance
+}
+// << events-source-model
