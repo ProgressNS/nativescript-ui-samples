2e35dd836dca4389736cb76b8890b1896b6f569a
karaahmed <karaahmed@gmail.com>
2016-05-25 11:46:37 +0300

    Fixed scatter series snipped tags

--- sdk/app/chart/series/scatter/scatter-series.ts
+++ sdk/app/chart/series/scatter/scatter-series.ts
@@ -1,6 +1,8 @@
+// >> binding-context-scatter
 import scatterModelModule = require("../../data-models/scatter-data-model");
 
 export function onPageLoaded(args){
     var page = args.object;
     page.bindingContext = new scatterModelModule.ScatterDataModel();
 }
+// << binding-context-scatter
