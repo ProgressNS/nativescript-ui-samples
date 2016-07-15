e83c737f75a85bdf8bc85b93a2159eadf9999f97
Deyan Ginev <deyan.ginev@telerik.com>
2016-07-15 11:20:48 +0300

    snippets added

--- sdk/app/feedback/getting-started/getting-started.ts
+++ sdk/app/feedback/getting-started/getting-started.ts
@@ -1,12 +1,16 @@
-import feedbackModule = require("nativescript-telerik-ui-pro/feedback");
+// >> feedback-import-module
+import {RadFeedback} from "nativescript-telerik-ui-pro/feedback";
+// << feedback-import-module
 import {FeedbackViewModel} from "./view-model";
 
 export function onPageLoaded(args) {
     var page = args.object;
     page.bindingContext = new FeedbackViewModel();
-    feedbackModule.RadFeedback.init("1234", "1234", "1234");
+    // >> feedback-init
+    RadFeedback.init("1234", "1234", "1234");
+    // << feedback-init
 }
 
 export function btnShowFeedbackTap(args) {
-    feedbackModule.RadFeedback.show();
+    RadFeedback.show();
 }
