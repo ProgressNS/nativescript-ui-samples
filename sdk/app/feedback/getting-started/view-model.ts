a00965335b050979f3aa4e3cce20cfcb5d6ebd2d
Deyan Ginev <deyan.ginev@telerik.com>
2016-08-08 16:42:45 +0300

    polishing

--- sdk/app/feedback/getting-started/view-model.ts
+++ sdk/app/feedback/getting-started/view-model.ts
@@ -2,9 +2,9 @@ export class FeedbackViewModel {
     private _title;
 
     constructor() {
-        this._title = "RadFeedback implements a straighforward mechanism for gathering user feedback about your mobile application." + 
-        "RadFeedback is seamlessly integrated with Telerik platform but can be customized to work with a third-party backend service." +
-        "Tap on the `Send feedback` button to activate the demonstration. Note that no connection to a backend-service is established.";
+        this._title = "AppFeedback implements a straighforward mechanism for gathering user feedback about your mobile application. " + 
+        "RadFeedback is seamlessly integrated with Telerik platform but can be customized to work with a third-party backend service. " +
+        "Tap on the `Start feedback` button to activate the demonstration. Note that no connection to a backend-service is established. ";
     }
 
     get title() {
