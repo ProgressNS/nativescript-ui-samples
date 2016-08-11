c5e1bd59195ac38b51d1293c3b46550a26faf327
Vladimir Amiorkov <Vladimir.Amiorkov@telerik.com>
2016-08-11 13:53:33 +0300

    Fixed missing code snippet tag after typescript compile

--- sdk/app/sidedrawer/transitions/drawer-transitions-model.ts
+++ sdk/app/sidedrawer/transitions/drawer-transitions-model.ts
@@ -51,6 +51,5 @@ export class DrawerTransitionsModel {
     private setDrawerTransition(transition: drawerModule.DrawerTransitionBase) {
         let drawer: drawerModule.RadSideDrawer = <drawerModule.RadSideDrawer>frameModule.topmost().getViewById("sideDrawer");
         drawer.drawerTransition = transition;
-    }
-    // << sidedrawer-setting-transition
+    }// << sidedrawer-setting-transition
 }
