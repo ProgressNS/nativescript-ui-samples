090dad93180e76c65c6f5ee234c732b71bd36794
tgpetrov <Todor.Petrov@telerik.com>
2016-08-16 18:49:45 +0300

    fixed an issue with remaining keyboard on android

--- sdk/app/navigation/example-page.ts
+++ sdk/app/navigation/example-page.ts
@@ -2,6 +2,7 @@ import pageModule = require("ui/page");
 import actionBarModule = require("ui/action-bar");
 import applicationModule = require("application");
 import frameModule = require("ui/frame");
+import utilsModule = require("utils/utils");
 
 export class ExamplePage extends pageModule.Page {
 
@@ -10,7 +11,7 @@ export class ExamplePage extends pageModule.Page {
     public constructor() {
         super();
         var that = new WeakRef(this);
-        this.on("navigatingTo", function(args) {
+        this.on("navigatingTo", function (args) {
             that.get()._associatedExampleMeta = args.context;
         });
 
@@ -24,8 +25,12 @@ export class ExamplePage extends pageModule.Page {
         actionBar.title = this._associatedExampleMeta.title;
         if (applicationModule.android) {
             var navigationButton = new actionBarModule.NavigationButton();
-            navigationButton.on("tap", args =>
-            { frameModule.topmost().goBack() });
+            navigationButton.on("tap", args => {
+                if(actionBar._nativeView) {
+                    utilsModule.ad.dismissSoftInput(actionBar._nativeView);
+                }
+                frameModule.topmost().goBack();
+            });
             navigationButton.icon = "res://ic_arrow_back_black_24dp";
             actionBar.navigationButton = navigationButton;
 
