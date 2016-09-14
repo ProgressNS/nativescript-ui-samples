51374b3bf264f0bab14e08b491d59f94c7bc19fc
tgpetrov <Todor.Petrov@telerik.com>
2016-09-14 16:43:48 +0300

    added a few unknown types for an example

---
+++ sdk/app/dataform/platform-specifics/platform-specifics.d.ts
@@ -0,0 +1,20 @@
+declare class TKSolidFill {
+
+}
+
+declare class NSDateFormatter {
+
+}
+
+declare module java {
+    module text {
+        class SimpleDateFormat {
+
+        }
+    }
+    module util {
+        class Locale {
+            static US: Locale;
+        }
+    }
+}
