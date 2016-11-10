40f2acc9af2f41e258124aa30f1f6742b36f78c0
Vladimir Amiorkov <v.amiorkov@gmail.com>
2016-11-10 10:35:12 +0200

    Revert "added usage of group layouts to data form group example"
    This reverts commit d15f27d4c4df2728c39d763b2964b44bc7710886.

--- sdk/app/dataform/groups/groups.ts
+++ sdk/app/dataform/groups/groups.ts
@@ -1,5 +1,5 @@
-import viewModel = require("./../view-models/person-extended-model");
+import viewModel = require("./../view-models/person-model");
 export function onPageLoaded(args) {
     var page = args.object;
-    page.bindingContext = new viewModel.PersonExtendedViewModel();
+    page.bindingContext = new viewModel.PersonViewModel();
 }
