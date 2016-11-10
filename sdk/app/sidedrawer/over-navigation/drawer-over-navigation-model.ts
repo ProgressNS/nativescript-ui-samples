1b180545c4d4a30177b5104400c6f39269676c88
guliashki <adrian.guliashki@gmail.com>
2016-11-10 17:07:04 +0200

    Added open drawer btn

--- sdk/app/sidedrawer/over-navigation/drawer-over-navigation-model.ts
+++ sdk/app/sidedrawer/over-navigation/drawer-over-navigation-model.ts
@@ -1,6 +1,14 @@
+import drawerModule = require("nativescript-telerik-ui-pro/sidedrawer");
+import frameModule = require("ui/frame");
+
 export class DrawerOverNavigationModel {
     get exampleText() {
         return "RadSideDrawer can be shown over the navigation bar/action bar by using DrawerPage instead of Page." +
             " DrawerPage has a sideDrawer property which accepts RadSideDrawer with just drawerContent set. The main content is the child of the page.";
+    }
+
+    public onOpenDrawerTap() {
+        let sideDrawer: drawerModule.RadSideDrawer = <drawerModule.RadSideDrawer>( frameModule.topmost().getViewById("sideDrawer"));
+        sideDrawer.showDrawer();
     }
 }
