e9ca221da19e268d5e7e5bcd63bde2cb741bdc1d
Deyan Ginev <deyan.ginev@telerik.com>
2016-11-15 17:12:40 +0200

    working on swipe-actions examples

--- sdk/app/listview/swipe-actions/swipe-actions-multiple.ts
+++ sdk/app/listview/swipe-actions/swipe-actions-multiple.ts
@@ -13,28 +13,14 @@ export function onPageLoaded(args) {
     page.bindingContext = new viewModel.ViewModel();
 }
 
-// >> listview-swipe-action-thresholds
+// >> listview-swipe-action-multiple
 export function onCellSwiping(args: listViewModule.ListViewEventData) {
     var swipeLimits = args.data.swipeLimits;
     var swipeView = args['swipeView'];
     var mainView = args['mainView'];
-    var leftItem = swipeView.getViewById('mark-view');
-    var rightItem = swipeView.getViewById('delete-view');
+    var leftItem = swipeView.getViewById('left-stack');
+    var rightItem = swipeView.getViewById('right-stack');
 
-    if (args.data.x > args.data.swipeLimits.threshold && !animationApplied) {
-        console.log("Notify perform left action");
-        leftItem.getViewById('mark-text').animate({
-            rotate: 360,
-            duration: 1000
-        });
-        animationApplied = true;
-    } else if (args.data.x < -args.data.swipeLimits.threshold && !animationApplied) {
-        rightItem.getViewById('delete-text').animate({
-            rotate: -360,
-            duration: 1000
-        });
-        animationApplied = true;
-    }
     if (args.data.x > 0) {
         var leftDimensions = viewModule.View.measureChild(
             leftItem.parent,
@@ -48,21 +34,19 @@ export function onCellSwiping(args: listViewModule.ListViewEventData) {
             rightItem,
             utilsModule.layout.makeMeasureSpec(Math.abs(args.data.x), utilsModule.layout.EXACTLY),
             utilsModule.layout.makeMeasureSpec(mainView.getMeasuredHeight(), utilsModule.layout.EXACTLY));
-            
+
         viewModule.View.layoutChild(rightItem.parent, rightItem, mainView.getMeasuredWidth() - rightDimensions.measuredWidth, 0, mainView.getMeasuredWidth(), rightDimensions.measuredHeight);
     }
 }
-// << listview-swipe-action-thresholds-notify
+// << listview-swipe-action-multiple-notify
 
-// >> listview-swipe-action-thresholds-limits
+// >> listview-swipe-action-multiple-limits
 export function onSwipeCellStarted(args: listViewModule.ListViewEventData) {
     var swipeLimits = args.data.swipeLimits;
-    var swipeView = args['object'];
-    var leftItem = swipeView.getViewById('mark-view');
-    var rightItem = swipeView.getViewById('delete-view');
-    swipeLimits.left = swipeLimits.right = args.data.x > 0 ? leftItem.getMeasuredWidth() * 2 : rightItem.getMeasuredWidth() * 2;
+    swipeLimits.threshold = args['mainView'].getMeasuredWidth() * 0.2; // 20% of whole width
+    swipeLimits.left = swipeLimits.right = args['mainView'].getMeasuredWidth() * 0.7 //70% of whole width
 }
-// << listview-swipe-action-release-limits
+// << listview-swipe-action-multiple-limits
 
 export function onSwipeCellFinished(args: listViewModule.ListViewEventData) {
     if (args.data.x > 200) {
@@ -75,13 +59,12 @@ export function onSwipeCellFinished(args: listViewModule.ListViewEventData) {
 
 export function onLeftSwipeClick(args: listViewModule.ListViewEventData) {
     var listView = <listViewModule.RadListView>frameModule.topmost().currentPage.getViewById("listView");
-    console.log("Left swipe click");
+    console.log("Button clicked: " + args.object.id + " for item with index: " + listView.items.indexOf(args.object.bindingContext));
     listView.notifySwipeToExecuteFinished();
 }
 
 export function onRightSwipeClick(args) {
     var listView = <listViewModule.RadListView>frameModule.topmost().currentPage.getViewById("listView");
-    console.log("Right swipe click");
-    var viewModel: viewModel.ViewModel = <viewModel.ViewModel>listView.bindingContext;
-    viewModel.dataItems.splice(viewModel.dataItems.indexOf(args.object.bindingContext), 1);
+    console.log("Button clicked: " + args.object.id + " for item with index: " + listView.items.indexOf(args.object.bindingContext));
+    listView.notifySwipeToExecuteFinished();
 }
