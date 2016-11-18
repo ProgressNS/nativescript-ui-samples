ecb82f2acb8aed83546dab22c8b039f839b52f69
tgpetrov <Todor.Petrov@telerik.com>
2016-11-18 17:53:17 +0200

    removed some redundant code

--- sdk/app/dataform/view-models/person-model-2.ts
+++ sdk/app/dataform/view-models/person-model-2.ts
@@ -1,37 +1,14 @@
-import fs = require("file-system");
 import observable = require("data/observable");
+
+var person = require("./person-model-2.json");
+var metadata = require("./person-metadata.json");
 
 export class PersonModel2 extends observable.Observable {
 
     constructor() {
         super();
-        
-        var documents = fs.knownFolders.currentApp();
-        var jsonPersonFile = documents.getFile('dataform/view-models/person-model-2.json');
-        var that = new WeakRef(this);
-        jsonPersonFile.readText()
-            .then(function (content) {
-                try {
-                    var person = JSON.parse(content);
-                    that.get().set("person", person);
-                } catch (err) {
-                    throw new Error('Could not parse JSON file');
-                }
-            }, function (error) {
-                throw new Error('Could not read JSON file');
-            });
 
-        var jsonMetadataFile = documents.getFile('dataform/view-models/person-metadata.json');
-        jsonMetadataFile.readText()
-            .then(function (content) {
-                try {
-                    var metadata = JSON.parse(content);
-                    that.get().set("personMetadata", metadata);
-                } catch (err) {
-                    throw new Error('Could not parse JSON file');
-                }
-            }, function (error) {
-                throw new Error('Could not read JSON file');
-            });
+        this.set("person", person);
+        this.set("personMetadata", metadata);
     }
 }
