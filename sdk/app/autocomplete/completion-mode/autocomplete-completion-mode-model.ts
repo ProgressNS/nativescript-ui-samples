032b5343733303a3d5d1d7971d624d8bc4714b4f
Vladimir Amiorkov <v.amiorkov@gmail.com>
2017-05-31 10:00:14 +0300

    Added new examples to AutoComplete (#184)

--- sdk/app/autocomplete/completion-mode/autocomplete-completion-mode-model.ts
+++ sdk/app/autocomplete/completion-mode/autocomplete-completion-mode-model.ts
@@ -1,9 +1,9 @@
 import { ObservableArray } from "tns-core-modules/data/observable-array";
-import autocompleteModule = require("nativescript-telerik-ui-pro/autocomplete");
+import { TokenModel } from "nativescript-telerik-ui-pro/autocomplete";
 
 export class ViewModel {
 
-    private _items: ObservableArray<autocompleteModule.TokenModel>;
+    private _items: ObservableArray<TokenModel>;
     private autocmp;
     private countries = ["Australia", "Albania", "Austria", "Argentina", "Maldives", "Bulgaria", "Belgium", "Cyprus", "Italy", "Japan",
         "Denmark", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland",
@@ -22,10 +22,10 @@ export class ViewModel {
     }
 
     private initDataItems() {
-        this._items = new ObservableArray<autocompleteModule.TokenModel>();
+        this._items = new ObservableArray<TokenModel>();
 
         for (var i = 0; i < this.countries.length; i++) {
-            this._items.push(new autocompleteModule.TokenModel(this.countries[i], undefined));
+            this._items.push(new TokenModel(this.countries[i], undefined));
         }
     }
 
