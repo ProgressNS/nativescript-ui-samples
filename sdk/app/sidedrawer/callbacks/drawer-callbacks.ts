ef762990e578577eb90e55081e0c83b8d4bc5e3f
Vladimir Amiorkov <v.amiorkov@gmail.com>
2017-08-31 17:44:41 +0300

    Added args type to events callbacks

--- sdk/app/sidedrawer/callbacks/drawer-callbacks.ts
+++ sdk/app/sidedrawer/callbacks/drawer-callbacks.ts
@@ -1,5 +1,6 @@
 import callbacksModelModule = require("./drawer-callbacks-model");
 import frameModule = require("tns-core-modules/ui/frame");
+import { DrawerStateChangingEventArgs, DrawerStateChangedEventArgs } from 'nativescript-telerik-ui-pro/sidedrawer';
 
 var drawerCallbacksModel : callbacksModelModule.DrawerCallbacksModel;
 
@@ -13,23 +14,23 @@ export function pageLoaded(args) {
 
 }
 // >> sidedrawer-events-handlers
-export function onDrawerClosed(args) {
+export function onDrawerClosed(args: DrawerStateChangedEventArgs) {
     drawerCallbacksModel.onDrawerClosed();
 }
 
-export function onDrawerClosing(args) {
+export function onDrawerClosing(args: DrawerStateChangingEventArgs) {
     drawerCallbacksModel.onDrawerClosing();
 }
 
-export function onDrawerOpened(args) {
+export function onDrawerOpened(args: DrawerStateChangedEventArgs) {
   drawerCallbacksModel.onDrawerOpened();
 }
 
-export function onDrawerOpening(args) {
+export function onDrawerOpening(args: DrawerStateChangingEventArgs) {
     drawerCallbacksModel.onDrawerOpening();
 }
 
-export function onDrawerPan(args) {
+export function onDrawerPan(args: DrawerStateChangedEventArgs) {
     drawerCallbacksModel.onDrawerPan();
 }
 // << sidedrawer-events-handlers
