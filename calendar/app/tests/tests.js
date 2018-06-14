864047da26d73dfc7abf3748f2a643e0e04835da
Dimitar Tachev <Dimitar.Tachev@progress.com>
2018-06-14 19:03:56 +0300

    chore: fix typings

--- calendar/app/tests/tests.js
+++ calendar/app/tests/tests.js
@@ -1,7 +1,4 @@
-import { RadCalendar } from "../../../src/index";
-
 var calendarModule = require("nativescript-ui-calendar");
-var uiCalendar = new calendarModule.RadCalendar();
 
 describe("greet function", function() {
    
